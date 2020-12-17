cab2af7406fefdecd98a7167e19dc8621c247d93
Ahmad Rohada <rohada987@gmail.com>
2020-12-17 09:59:23 +0700

    tes

--- config/Koneksi.php
+++ config/Koneksi.php
@@ -3,9 +3,9 @@ class Connect {
 	protected static $_connection;
 	public static function getConnection(){
 		if(!self::$_connection){
-			$dbhost = '10.1.1.1:1946';
-			$dbuser = 'pak';
-			$dbpassword = 'Pak123!@#';
+			$dbhost = 'localhost';
+			$dbuser = 'root';
+			$dbpassword = '';
 			$dbname ='pak_baru';
 			self::$_connection = @mysql_connect($dbhost, $dbuser, $dbpassword);
 			if(!self::$_connection){
@@ -24,7 +24,7 @@ class Connect {
 			$dbhost = 'localhost';
 			$dbuser = 'root';
 			$dbpassword = '';
-			$dbname ='db_opak';
+			$dbname ='pak_baru';
 			self::$_connection = @mysql_connect($dbhost, $dbuser, $dbpassword);
 			if(!self::$_connection){
 				throw new Exception('Gagal melalukan koneksi ke database. '.mysql_error());
